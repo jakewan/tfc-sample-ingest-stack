b48588878ceed7da4692bc0dca7d2bd1a103208e
Jacob Wan <jacobwan840@gmail.com>
2021-03-10 14:59:04 -0800

    Add eventhub

--- src/main.tf
+++ src/main.tf
@@ -6,3 +6,18 @@ resource "azurerm_resource_group" "beacons" {
   name     = "rg-beacons-${var.environment_name}"
   location = "West US"
 }
+
+resource "azurerm_eventhub_namespace" "beacons" {
+  name                = "nsBeacons-${var.environment_name}"
+  location            = azurerm_resource_group.beacons.location
+  resource_group_name = azurerm_resource_group.beacons.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_eventhub" "name" {
+  name                = "foo-${var.environment_name}"
+  location            = azurerm_resource_group.beacons.location
+  resource_group_name = azurerm_resource_group.beacons.name
+  partition_count     = 2
+  message_retention   = 1
+}
