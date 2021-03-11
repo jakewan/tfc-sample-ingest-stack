bf4957297aabdd14dcc8f358c2f56fabc72bae62
Jacob Wan <jacobwan840@gmail.com>
2021-03-11 10:21:54 -0800

    WIP module

--- src/modules/beacons/messaging/main.tf
+++ src/modules/beacons/messaging/main.tf
@@ -1,10 +1,14 @@
 resource "azurerm_eventhub_namespace" "beacons" {
-   name                = "nsBeacons-${var.environment_name}"
-   location            = var.location
-   resource_group_name = var.resource_group_name
-   sku                 = "Standard"
- }
+  name                = "nsBeacons-${var.environment_name}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+}
 
 resource "azurerm_eventhub" "eventhub_1" {
-    name = "eventhub-${var.environment_name}-1"
+  name                = "eventhub-${var.environment_name}-1"
+  resource_group_name = var.resource_group_name
+  namespace_name      = azurerm_eventhub_namespace.beacons.name
+  partition_count     = 2
+  message_retention   = 1
 }
