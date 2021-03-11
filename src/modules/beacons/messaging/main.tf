de0b3a6b9242f982fea71fb5b5f113602ec23e60
Jacob Wan <jacobwan840@gmail.com>
2021-03-11 10:16:48 -0800

    WIP module

--- src/modules/beacons/messaging/main.tf
+++ src/modules/beacons/messaging/main.tf
@@ -1,3 +1,10 @@
-resource "azurerm_eventhub" "eventhub_1" {
+resource "azurerm_eventhub_namespace" "beacons" {
+   name                = "nsBeacons-${var.environment_name}"
+   location            = var.location
+   resource_group_name = var.resource_group_name
+   sku                 = "Standard"
+ }
 
+resource "azurerm_eventhub" "eventhub_1" {
+    name = "eventhub-${var.environment_name}-1"
 }
